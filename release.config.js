254204182e122303029d059d1b27f260b209d41e
Dave Brotherstone <dave.brotherstone@soundcloud.com>
2021-05-15 22:38:50 +0200

    build: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,45 @@
+const noteKeywords = ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'];
+
+module.exports = {
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords,
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords,
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
+    ['@semantic-release/npm'],
+    ['@semantic-release/github'],
+  ],
+};
